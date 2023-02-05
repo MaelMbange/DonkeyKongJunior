9c14fd54a2c791f42920e245a146cd48e48c4eae
Mael Mbange <maelmbange21@gmail.com>
2023-02-05 21:52:57 +0100

    exercice3

--- threadLabo1/main.cpp
+++ threadLabo1/main.cpp
@@ -10,13 +10,20 @@ void Exercice4(void);
 
 void* thread1(void* );
 void* thread2(void* );
+void* thread3_1(void* );
+void* thread3_2(void* );
+void* thread3_3(void* );
 
 void handlerAlarm(int);
+void handlerUSR1(int);
+void handlerUSR2(int);
 
 int main(int argc, char* argv[])
 {
     // Exercice1();
-    Exercice2();
+    // Exercice2();
+    Exercice3();
+
     return 0; 
 }
 
@@ -31,7 +38,6 @@ void * thread1(void *i){
     nanosleep(&time,NULL);
     pthread_exit(NULL);
 }
-
 void* thread2(void* i){
     fprintf(stdout,"Debut Thread= %lld\n",pthread_self());
 
@@ -39,11 +45,49 @@ void* thread2(void* i){
     sigemptyset(&sig);
     sigaddset(&sig,SIGALRM);
     sigprocmask(SIG_UNBLOCK,&sig,NULL);
+
     alarm(5);
+    // fprintf(stdout,"Debut pause\n");
     pause();
+    // fprintf(stdout,"Fin pause\n");
+
     fprintf(stdout,"Fin Thread= %lld\n",pthread_self());
     pthread_exit(NULL);
 }
+void* thread3_1(void* ){
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask,SIGALRM);
+    sigprocmask(SIG_UNBLOCK,&mask,NULL);
+
+    fprintf(stdout,"\033[91mDebut Thread1= %lld\n\033[0m",pthread_self());
+    pause();
+    fprintf(stdout,"\033[91mFin Thread1= %lld\n\033[0m",pthread_self());
+    pthread_exit(NULL);
+}
+void* thread3_2(void* ){
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask,SIGUSR1);
+    sigprocmask(SIG_UNBLOCK,&mask,NULL);
+
+    fprintf(stdout,"\033[92mDebut Thread2= %lld\n\033[0m",pthread_self());
+    pause();
+    fprintf(stdout,"\033[92mFin Thread2= %lld\n\033[0m",pthread_self());
+    pthread_exit(NULL);
+}
+void* thread3_3(void* ){
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask,SIGUSR2);
+    sigprocmask(SIG_UNBLOCK,&mask,NULL);
+
+    fprintf(stdout,"\033[93mDebut Thread3= %lld\n\033[0m",pthread_self());
+    alarm(5);
+    pause();
+    fprintf(stdout,"\033[93mFin Thread3= %lld\n\033[0m",pthread_self());
+    pthread_exit(NULL);
+}   
 
 void Exercice1(void){
     pthread_t vec[4];    
@@ -60,8 +104,6 @@ void Exercice1(void){
         pthread_join(x,NULL);
     }   
 }
-
-
 void Exercice2(void){
     struct sigaction sig;
     sig.sa_flags = 0;
@@ -69,13 +111,59 @@ void Exercice2(void){
     sigemptyset(&sig.sa_mask);
     sigaction(SIGALRM,&sig,NULL);
 
-    sigprocmask(SIG_BLOCK,&sig.sa_mask,NULL);
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask,SIGALRM);
+    sigprocmask(SIG_BLOCK,&mask,NULL);
 
-    pthread_create();
+    pthread_t thread;
+    pthread_create(&thread,NULL,thread2,NULL);
+
+    pthread_join(thread,NULL);
 
 }
 void Exercice3(void){
+    pthread_t threads[3];
 
+    struct sigaction signal;
+    signal.sa_flags = 0;
+    signal.sa_handler = handlerAlarm;
+    sigemptyset(&signal.sa_mask);
+    sigaction(SIGALRM,&signal,NULL);
+
+    signal.sa_flags = 0;
+    signal.sa_handler = handlerUSR1;
+    sigemptyset(&signal.sa_mask);
+    sigaction(SIGUSR1,&signal,NULL);
+
+    signal.sa_flags = 0;
+    signal.sa_handler = handlerUSR2;
+    sigemptyset(&signal.sa_mask);
+    sigaction(SIGUSR2,&signal,NULL);
+
+    sigset_t mask;
+    sigfillset(&mask);
+    sigprocmask(SIG_BLOCK,&mask,NULL);
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        if(i == 0)
+            pthread_create(&threads[0],NULL,thread3_1,NULL);
+        else if(i == 1)
+            pthread_create(&threads[1],NULL,thread3_2,NULL);
+        else if(i == 2)
+            pthread_create(&threads[2],NULL,thread3_3,NULL);
+    }
+    struct timespec time{2,0};
+    nanosleep(&time,NULL);
+    pthread_kill(threads[1],SIGUSR1);
+    pthread_kill(threads[2],SIGUSR2);
+
+    for(pthread_t& x: threads)
+    {
+        pthread_join(x,NULL);
+    } 
+    
 }
 void Exercice4(void){
 
@@ -85,4 +173,11 @@ void Exercice4(void){
 void handlerAlarm(int)
 {
     fprintf(stdout,"Alarm Thread= %lld\n",pthread_self());
+}
+
+void handlerUSR1(int){
+    fprintf(stdout,"User1 Thread= %lld\n",pthread_self());
+}
+void handlerUSR2(int){
+    fprintf(stdout,"User2 Thread= %lld\n",pthread_self());
 }
