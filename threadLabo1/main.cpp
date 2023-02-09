fa5511bf19b9cbf506a176cb1cc293a04dd8f264
Mael Mbange <maelmbange21@gmail.com>
2023-02-09 16:23:17 +0100

    exercices 1->3

--- threadLabo1/main.cpp
+++ threadLabo1/main.cpp
@@ -26,7 +26,10 @@ int main(int argc, char* argv[])
 
     return 0; 
 }
-
+void Bip(void){
+    fprintf(stdout,"\a");
+    fflush(stdout);
+}
 void * thread1(void *i){
     // std::cout << "Debut Thread= "<< pthread_self() << std::endl;
     // std::cout << "value= " << *(int*)i << std::endl;
@@ -60,9 +63,9 @@ void* thread3_1(void* ){
     sigaddset(&mask,SIGALRM);
     sigprocmask(SIG_UNBLOCK,&mask,NULL);
 
-    fprintf(stdout,"\033[91mDebut Thread1= %lld\n\033[0m",pthread_self());
+    fprintf(stdout,"\033[91mDebut Thread1= %lld\033[0m\n",pthread_self());
     pause();
-    fprintf(stdout,"\033[91mFin Thread1= %lld\n\033[0m",pthread_self());
+    fprintf(stdout,"\033[91mFin Thread1= %lld\033[0m\n",pthread_self());
     pthread_exit(NULL);
 }
 void* thread3_2(void* ){
@@ -71,9 +74,9 @@ void* thread3_2(void* ){
     sigaddset(&mask,SIGUSR1);
     sigprocmask(SIG_UNBLOCK,&mask,NULL);
 
-    fprintf(stdout,"\033[92mDebut Thread2= %lld\n\033[0m",pthread_self());
+    fprintf(stdout,"\033[92mDebut Thread2= %lld\033[0m\n",pthread_self());
     pause();
-    fprintf(stdout,"\033[92mFin Thread2= %lld\n\033[0m",pthread_self());
+    fprintf(stdout,"\033[92mFin Thread2= %lld\033[0m\n",pthread_self());
     pthread_exit(NULL);
 }
 void* thread3_3(void* ){
@@ -82,10 +85,10 @@ void* thread3_3(void* ){
     sigaddset(&mask,SIGUSR2);
     sigprocmask(SIG_UNBLOCK,&mask,NULL);
 
-    fprintf(stdout,"\033[93mDebut Thread3= %lld\n\033[0m",pthread_self());
+    fprintf(stdout,"\033[96mDebut Thread3= %lld\033[0m\n",pthread_self());
     alarm(5);
     pause();
-    fprintf(stdout,"\033[93mFin Thread3= %lld\n\033[0m",pthread_self());
+    fprintf(stdout,"\033[96mFin Thread3= %lld\033[0m\n",pthread_self());
     pthread_exit(NULL);
 }   
 
@@ -123,8 +126,11 @@ void Exercice2(void){
 
 }
 void Exercice3(void){
+
     pthread_t threads[3];
 
+    void*(*tab[3])(void*) {thread3_1,thread3_2,thread3_3};
+    
     struct sigaction signal;
     signal.sa_flags = 0;
     signal.sa_handler = handlerAlarm;
@@ -143,19 +149,24 @@ void Exercice3(void){
 
     sigset_t mask;
     sigfillset(&mask);
+    sigdelset(&mask,SIGINT);
     sigprocmask(SIG_BLOCK,&mask,NULL);
 
     for (size_t i = 0; i < 3; i++)
     {
-        if(i == 0)
+        /*if(i == 0)
             pthread_create(&threads[0],NULL,thread3_1,NULL);
         else if(i == 1)
             pthread_create(&threads[1],NULL,thread3_2,NULL);
         else if(i == 2)
-            pthread_create(&threads[2],NULL,thread3_3,NULL);
+            pthread_create(&threads[2],NULL,thread3_3,NULL);*/
+        pthread_create(&threads[i],NULL,tab[i],NULL);
     }
-    struct timespec time{2,0};
+    struct timespec time{1,0};
     nanosleep(&time,NULL);
+    fprintf(stdout,"\033[0m==============================\n");
+    nanosleep(&time,NULL);
+
     pthread_kill(threads[1],SIGUSR1);
     pthread_kill(threads[2],SIGUSR2);
 
@@ -163,6 +174,14 @@ void Exercice3(void){
     {
         pthread_join(x,NULL);
     } 
+
+    /*time = {0,500'000'000};
+    for (size_t i = 0; i < 10; i++)
+    {
+        Bip();
+        nanosleep(&time,NULL);
+    }*/
+    
     
 }
 void Exercice4(void){
@@ -170,9 +189,8 @@ void Exercice4(void){
 }
 
 
-void handlerAlarm(int)
-{
-    fprintf(stdout,"Alarm Thread= %lld\n",pthread_self());
+void handlerAlarm(int){
+    fprintf(stdout,"\033[5m\aAlarm\033[0m Thread= %lld\n",pthread_self()); 
 }
 
 void handlerUSR1(int){
